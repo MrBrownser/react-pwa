adc5eed19825f23ddcd763a462126572bef37159
Adria Castany <adria.castany@haufe.com>
2018-02-04 22:18:17 +0100

    Add style and remove marker from map

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import * as mapStyles from "../../gmapsStyle.json";
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultCenter={{ lat: 41.377900, lng: 2.173768 }}
+    defaultOptions={{ styles: mapStyles }}
     >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
   </GoogleMap>
 ));
 
@@ -14,7 +15,6 @@ class Greeting extends React.Component {
   render () {
     return (
       <MyMapComponent
-        isMarkerShown
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAU_mIkviIhRwim0rjXlkpcmq4IBy1oLg8&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
