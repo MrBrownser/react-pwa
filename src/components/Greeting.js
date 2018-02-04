1c43dd6c8d40948b728f333b3044a81b555052ff
Adria Castany <adria.castany@haufe.com>
2018-02-04 19:10:03 +0100

    WIP map still not working

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import {Card, CardTitle, CardText} from 'material-ui/Card';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+));
 
 class Greeting extends React.Component {
   render () {
     return (
-      <div>
-        <Card>
-          <CardTitle title="Hello! World"/>
-          <CardText>
-            You will have a work:
-            <ul>
-              <li>Web Manifest for installing</li>
-              <li>Service Worker for caching and offline</li>
-              <li>Application Shell powered by <a href="https://material-ui.com">material-ui</a></li>
-              <li>PRPL pattern by code splitting</li>
-              <li>Opt in ES2015</li>
-            </ul>
-          </CardText>
-        </Card>
-      </div>
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     )
   }
 };
