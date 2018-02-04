fe9ec546860f18d6f27d8f1fc451d1165fdc3014
Adria Castany <adria.castany@haufe.com>
2018-02-04 22:06:32 +0100

    Having the thing working, yay

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -15,7 +15,7 @@ class Greeting extends React.Component {
     return (
       <MyMapComponent
         isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAU_mIkviIhRwim0rjXlkpcmq4IBy1oLg8&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
